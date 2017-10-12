7e21ef21c3c8cb9053f7ef2bc54396a332adca30
Teng Wang <tengwang@zwdai.com>
2017-10-12 18:12:02 +0800

    vue test

---
+++ vue/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Test1 from '@/components/Test1'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: HelloWorld
+    },
+    {
+      path: '/test1',
+      name: 'test1',
+      component: Test1
+    }
+  ]
+})
